42fa637207cdf72463be660eb0c5e92a98848465
Eugene Niyitanga <eugene.niyitanga2015@gmail.com>
2017-02-22 12:12:22 +0200

    created a clickBox game

--- app/assets/javascripts/jackhammer.js
+++ app/assets/javascripts/jackhammer.js
@@ -1,1 +1,16 @@
+var winningBox = 5; // Picks a random number between 1 and 3:
+
+function jackhammer(clickTimes) {
+
+  if (clickTimes === winningBox) {
+    alert("You win!");
+  } else {
+    console.log("wrong");
+  }
+
+// }
+//   console.log("wrong");
+//   }
+
+}
 
